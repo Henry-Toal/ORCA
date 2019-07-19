5941d5fdfa77aae9a591e24fa6931c01b751bb92
Henry-Toal <52763195+Henry-Toal@users.noreply.github.com>
2019-07-19 11:50:43 -0800

    Add files via upload

--- ORCA_shark200_main_script.py
+++ ORCA_shark200_main_script.py
@@ -11,6 +11,7 @@ import copy
 import csv
 import os
 import shark_200_meter_settings
+import shark_200_readings_blocks
 
 
     
@@ -51,28 +52,16 @@ designed to be run on start-up and will automatically produce .csv files (with c
 #--------------------------------------------------------------------------------------
 # - Create variable-length csv files? More/less than  one day
 # - Setup a seperate logging file for each of the meters.
+# - Make sure that the new .csv files are created at the beginning of the hour and aren't held up by connection issues.
 
 
-
-# - At some point, change the readings section instructions to include more than the 1000-to-1053 range
+# - At some point, change the readings section instructions to include more than the 1000-to-1059 range
 # -- When that's done, change the output (to_csv) dataframe to be a seperate main dataframe that the
 #....block-specific dataframes are appended to.
 #--------------------------------------------------------------------------------------    
     
     
-    # INPUTS:
-    
-    # Enter Device IP Address (host) as (str) and port number as (int).
-    # Choose your timestep (in seconds) and number of decimal places for the output data.
-#--------------------------------------------------------------------------------------
 
-host = '75.127.189.115' #(str) # IP address of the power meter 
-port = 503 #(int)              # port the device is using for modbus protocol. Usually 502 or 503. 
-
-timestep = 1 #(int)            # The interval, in seconds, between data measurements. Below 5 seconds not recommended. 
-decimal_places = 3 #(int)      # The number of decimal places the final .csv file will be rounded to.
-
-meter_name = 'shark_1'
 #--------------------------------------------------------------------------------------
 
 
@@ -122,37 +111,37 @@ readings =    ((                       # From here you can edit which values fro
     
     # !!! DO NOT CHANGE !!!
 #--------------------------------------------------------------------------------------
-primary_readings_columns = (('timestamp',
-                                 'Volts A-N',
-                                 'Volts B-N',
-                                 'Volts C-N',
-                                 'Volts A-B',
-                                 'Volts B-C',
-                                 'Volts C-A',
-                                 'Amps A',
-                                 'Amps B',
-                                 'Amps C',
-                                 'Watts 3-Ph total',
-                                 'VARs 3-Ph total',
-                                 'VAs 3-Ph total',
-                                 'Power Factor 3-Ph total',
-                                 'Frequency',
-                                 'Neutral Current',
-                                 'Watts Phase A',
-                                 'Watts Phase B',
-                                 'Watts Phase C',
-                                 'VARs Phase A',
-                                 'VARs, Phase B',
-                                 'VARs Phase C',
-                                 'VAs Phase A',
-                                 'VAs Phase B',
-                                 'VAs Phase C',
-                                 'Power Factor Phase A',
-                                 'Power Factor Phase B',
-                                 'Power Factor Phase C',
-                                 'Symmetrical Component Magnitude 0 Seq',
-                                 'Symmetrical Component Magnitude + Seq',
-                                 'Symmetrical Component Magnitude - Seq'))
+#primary_readings_columns = (('timestamp',
+#                                 'Volts A-N',
+#                                 'Volts B-N',
+#                                 'Volts C-N',
+#                                 'Volts A-B',
+#                                 'Volts B-C',
+#                                 'Volts C-A',
+#                                 'Amps A',
+#                                 'Amps B',
+#                                 'Amps C',
+#                                 'Watts 3-Ph total',
+#                                 'VARs 3-Ph total',
+#                                 'VAs 3-Ph total',
+#                                 'Power Factor 3-Ph total',
+#                                 'Frequency',
+#                                 'Neutral Current',
+#                                 'Watts Phase A',
+#                                 'Watts Phase B',
+#                                 'Watts Phase C',
+#                                 'VARs Phase A',
+#                                 'VARs, Phase B',
+#                                 'VARs Phase C',
+#                                 'VAs Phase A',
+#                                 'VAs Phase B',
+#                                 'VAs Phase C',
+#                                 'Power Factor Phase A',
+#                                 'Power Factor Phase B',
+#                                 'Power Factor Phase C',
+#                                 'Symmetrical Component Magnitude 0 Seq',
+#                                 'Symmetrical Component Magnitude + Seq',
+#                                 'Symmetrical Component Magnitude - Seq'))
 
 #--------------------------------------------------------------------------------------
 
@@ -255,39 +244,16 @@ def checkConnection(host):
 
 
 def main():  # Primary function that contains the data collection loop
+
+        # Logging
+    #---------------------------------------------------------------------------------
+    logging.basicConfig(filename='log.txt', format='%(asctime)s-%(levelname)s: %(message)s', level=logging.INFO)                     
+    #--------------------------------------------------------------------------------- 
     
     
-    
-    
-        # Importing global variables
-    #---------------------------------------------------------------------------------
-    global host     # Import the host and port varibale into the main() function.
-    global port
-    global readings # The list of desired values to be measured
-    global decimal_places
-    global primary_readings_columns
-    #---------------------------------------------------------------------------------
-    
-        # Removing commas from the column names list(s) and readings list
-    #---------------------------------------------------------------------------------
-    readings = [name.replace(',', '') for name in readings]
-    
-    primary_readings_columns = [name.replace(',', '') for name in primary_readings_columns]
-    #---------------------------------------------------------------------------------
-    
-        # Filtering out any unwanted characters in the meter name
-    #---------------------------------------------------------------------------------
-    #---------------------------------------------------------------------------------
-    
-        # Insert timestamp column
-    #---------------------------------------------------------------------------------
-    readings.insert(0, 'timestamp') # Makes sure the data has a column for the Unix time stamp
-    #---------------------------------------------------------------------------------
-        
     
     ###################################################################################
     ###################################################################################
-    
     while True:     # Data collection loop
         
             # Start Timer
@@ -295,101 +261,135 @@ def main():  # Primary function that contains the data collection loop
         start = timeit.default_timer()
         ##############################
             
+        
+               
+        for meter_name, host, port, timestep, decimal_places, readings in shark_200_meter_settings.settings:
+        
+            while True:
+                if checkConnection(host) == True:
+                    print('connection to ' + meter_name + ' good')
+                    break
+                else:
+                    logging.error('Could not connect to {} at '{}'.'.format(meter_name, host) + 'Retrying...')
+                    time.sleep(3)
+        
+        
             
-        while True:
-            if checkConnection(host) == True:
-                print('connection good')
-                break
+                # Timestamp
+            #---------------------------------------------------------------------------------   
+            timestamp = time.time()  # Making the timestamp
+            #---------------------------------------------------------------------------------   
+            
+                # Insert timestamp column
+            #---------------------------------------------------------------------------------
+            if readings[0] != 'timestamp':
+                readings.insert(0, 'timestamp') # Makes sure the data has a column for the Unix time stamp
+            #---------------------------------------------------------------------------------
+            
+                # Filtering out any unwanted commas from the list of desired columns
+            #---------------------------------------------------------------------------------
+            readings = [name.replace(',', '') for name in readings]
+            #---------------------------------------------------------------------------------    
+                
+                # Creating the .csv file to be written to
+            #---------------------------------------------------------------------------------
+            now = datetime.datetime.now()
+            
+            file_name = (str(meter_name) + '_{}'*4 + '.csv').format(now.year, now.month, now.day, now.hour)
+            file_path = './data/' + file_name
+            #---------------------------------------------------------------------------------
+               
+               
+               
+                #Primary readings block -- Pages MM-2 to MM-3 of the shark200 user's manual
+            #---------------------------------------------------------------------------------
+            primary_readings_columns = shark_200_readings_blocks.primary_readings_block
+            primary_readings_columns = [name.replace(',', '') for name in primary_readings_columns]
+            
+            primary_readings_modbus_data = getModbusData(host, port, start_register=1000, end_register=1059)
+            
+            if primary_readings_modbus_data == None:
+                logging.error('Modbus query returned no data')
+                continue
             else:
-                print('connection error')
-                time.sleep(3)
-#            
+                try:
+                    primary_readings_data = format32BitFloat(primary_readings_modbus_data)
+                except:
+                    logging.error('format32BitFLoat() failed to format data'
+                                   + '\n' + 'Data Type: {}'.format(type(primary_readings_modbus_data)), exc_info=True)
+                              
             
+            temp_dict = {}     # Dictionary that will take in the new data on each loop and be cleared on each iteration.
             
+            for index, name in enumerate(primary_readings_columns):     # Here we loop through all the possible columns in this block and add them to the dictionary
+                if name == 'timestamp':
+                    temp_dict[name] = [timestamp]
+                else:
+                    temp_dict[name] = [round(primary_readings_data[index - 1], decimal_places)]     # Here we need to reduce the index by 1
+                                                                                                    #...to account for the added timestamp column
+                                                                
             
+            temp_df = pd.DataFrame(temp_dict)     # Make a pandas.DataFrame from the dictionary                 
+            temp_df = temp_df[readings]           # Filters out all columns that weren't specified in the 'readings' variable
             
-            # Timestamp
-        #---------------------------------------------------------------------------------   
-        timestamp = time.time()  # Making the timestamp
-        #---------------------------------------------------------------------------------   
-            
-            
-            # Creating the .csv file to be written to
-        #---------------------------------------------------------------------------------
-        now = datetime.datetime.now()
-        
-        file_name = ('shark200' + '_{}'*3 + '.csv').format(now.year, now.month, now.day)  
-        #---------------------------------------------------------------------------------
-           
-            
-            #Primary readings block -- Pages MM-2 to MM-3 of the shark200 user's manual
-        #---------------------------------------------------------------------------------
-        primary_readings_modbus_data = getModbusData(host, port, start_register=1000, end_register=1059)
-        primary_readings_data = format32BitFloat(primary_readings_modbus_data)
-        
-        temp_dict = {}     # Dictionary that will take in the new data on each loop and be cleared on each iteration.
-        
-        for index, name in enumerate(primary_readings_columns):     # Here we loop through all the possible columns in this block and add them to the dictionary
-            if name == 'timestamp':
-                temp_dict[name] = [timestamp]
-            else:
-                temp_dict[name] = [round(primary_readings_data[index - 1], decimal_places)]     # Here we need to reduce the index by 1
-                                                                                                #...to account for the added timestamp column
-                                                            
-        
-        temp_df = pd.DataFrame(temp_dict)     # Make a pandas.DataFrame from the dictionary                 
-        temp_df = temp_df[readings]           # Filters out all columns that weren't specified in the 'readings' variable
-        
-        if file_name not in os.listdir('.'):                                                       # '.' indicates 'current directory'
-            with open(file_name, 'a') as data_file:                                                # 'a' indicates 'append' to the file
-                temp_df[temp_df['timestamp'] == 0].to_csv(data_file, header=True, index=False)     # This is a quick and dirty way to write the column headers to the .csv
-                                                                                                   #...file while making sure they line up with the columns you want.
-                                
-        else:
-            with open(file_name, 'a') as data_file:                      # 'a' indicates 'append' to the file
-                temp_df.to_csv(data_file, header=False, index=False)     # This will account for every other loop iteration and append the data to the .csv file
-            
+            if 'data' not in os.listdir('.'):
+                os.mkdir('data')
+                if 'data' in os.listdir('.'):
+                    logging.info('Data directory successfully created')
+                else:
+                    logging.error('Error when creating data directory')
+                    continue
                 
             
-
-                                                        
-        
-        
-            
+            if file_name not in os.listdir('./data'):                                                # '.' indicates 'current directory'
+                with open(file_path, 'a') as data_file:                                              # 'a' indicates 'append' to the file
+                    temp_df[temp_df['timestamp'] == 0].to_csv(data_file, header=True, index=False)     # This is a quick and dirty way to write the column headers to the .csv          
+                if file_name in os.listdir('./data'):
+                    logging.info('New CSV file: ' + file_name + ' successfully created')                                                                                                               
+                                    
+            else:
+                with open(file_path, 'a') as data_file:                      # 'a' indicates 'append' to the file
+                    temp_df.to_csv(data_file, header=False, index=False)     # This will account for every other loop iteration and append the data to the .csv file
+                
+                    
             
             
             # End Timer
-        ##########################################    
+        ##########################################        
         stop = timeit.default_timer()
-        print('Time: ', stop - start)
-        print('')
-        print('success!')
-        print('Length of readings: {}'.format(len(readings)))
-        time.sleep(timestep)
         ##########################################
         
         
-    ###################################################################################
-    ###################################################################################
-        
+            # Controlling the length of the time.sleep() call
         #---------------------------------------------------------------------------------
+        run_time = stop - start
+        if (timestep - run_time) >= 0:
+            time.sleep(timestep - run_time)
+        #---------------------------------------------------------------------------------
+            
+            
+                                                            
+            
         
-    
-    
-
+            
+            
+                
+            # End Timer
+        ##########################################    
+        print('success!')
         
-
+        print('Time: ', stop - start)
+        print('')
+        
+        
+        
+        
+    ###################################################################################
+    ###################################################################################
     
-import shark_200_meter_settings
-
-settings = shark_200_meter_settings.settings
-
-
-
-
-#if __name__ == '__main__':
-#    main()
-
+        
+if __name__ == '__main__':
+    main()
 
 
 
